51ba3eb0e5ca289bc7c469fad884381650f11c9f
Michelle031 <mbaangel2803@gmail.com>
2022-06-06 07:26:47 +0100

    repo

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {BrowserRouter} from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     
-        <BrowserRouter basename="/rest-countries-api-project">
+        <BrowserRouter basename="/countries-api-project">
             <App />
         </BrowserRouter>
 );
